a90da222f593b4e1bd77a84b3e1dcbbf40672fa5
nasser amini <na3ramini.73@gmail.com>
2022-05-06 17:58:53 +0430

    reform structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: CodeBazApp()));
-}
-class CodeBazApp extends StatefulWidget {
-  const CodeBazApp({Key? key}) : super(key: key);
-
-  @override
-  State<CodeBazApp> createState() => _CodeBazAppState();
-}
-
-class _CodeBazAppState extends State<CodeBazApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
-    );
-  }
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+    ),
+  );
 }
